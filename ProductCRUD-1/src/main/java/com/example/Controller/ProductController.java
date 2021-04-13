de993595f13045f9ee80ef327274554d35433933
shanjana91 <shanjanam.91@gmail.com>
2021-04-13 15:09:33 +0530

    enhanced Swagger Documentation by adding certain Swagger annotations

--- ProductCRUD-1/src/main/java/com/example/Controller/ProductController.java
+++ ProductCRUD-1/src/main/java/com/example/Controller/ProductController.java
@@ -9,50 +9,58 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Entity.Product;
 import com.example.Service.ProductService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class ProductController {
-	
+
 	@Autowired
-	private ProductService service; //controller --> service
-	
+	private ProductService service; // controller --> service
+
+	@ApiOperation(value = "Insert Product Records", response = Product.class)
 	@PostMapping("/create")
 	public Product addProduct(@RequestBody Product product) {
 		return service.create(product);
 	}
-	
+
+	@ApiOperation(value = "Insert more than one Product Records", response = Product.class)
 	@PostMapping("/createProducts")
 	public List<Product> addProducts(@RequestBody List<Product> products) {
 		return service.createproducts(products);
 	}
-	
+
+	@ApiOperation(value = "Retrieve Product Records by ID", response = Product.class)
 	@GetMapping("/getById/{id}")
-	public Product getProductById(@PathVariable int id){
+	public Product getProductById(@PathVariable int id) {
 		return service.getProductByid(id);
 	}
-	
+
+	@ApiOperation(value = "Retrieve Product Record By name", response = Product.class)
 	@GetMapping("/get/{name}")
-	public Product getProductByName(@PathVariable String name){
+	public Product getProductByName(@PathVariable String name) {
 		return service.getProductByName(name);
 	}
-	
+
+	@ApiOperation(value = "Retrieve all Product Records", response = Product.class)
 	@GetMapping("/getproducts")
-	public List<Product> getProducts(){
+	public List<Product> getProducts() {
 		return service.getProducts();
 	}
-	
+
+	@ApiOperation(value = "Delete a Product Record", response = Product.class)
 	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable int id) {
+	public String deleteProduct(@PathVariable int id) {
 		return service.deleteProduct(id);
 	}
-	
+
+	@ApiOperation(value = "Update a Product Record", response = Product.class)
 	@PutMapping("/update/{id}")
-	public Product update(@RequestBody Product product) {
+	public Product updateProduct(@RequestBody Product product) {
 		return service.updateProduct(product);
 	}
 }
--- ProductCRUD-1/src/main/java/com/example/Entity/Product.java
+++ ProductCRUD-1/src/main/java/com/example/Entity/Product.java
@@ -4,15 +4,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import io.swagger.annotations.ApiModelProperty;
+
 //MODEL CLASS
 
 @Entity
 public class Product {
 	@Id
 	@GeneratedValue
+	@ApiModelProperty(notes = "Product ID" ,name = "id",required=true)
 	private int id;
+	@ApiModelProperty(notes = "Product Name" ,name = "name",required = true)
 	private String name;
+	@ApiModelProperty(notes = "Product quantity in stock" ,name = "quantity",required = true)
 	private int quantity;
+	@ApiModelProperty(notes = "Product price" ,name = "price",required = true)
 	private int price;
 	
 	
