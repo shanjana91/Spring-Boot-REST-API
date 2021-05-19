2144dd8f5bfc6c97a69a48cc8ce55e66a00f7105
shanjana91 <shanjanam.91@gmail.com>
2021-05-19 13:13:43 +0530

    application.java file updated

--- ProductCRUD-1/src/main/java/com/example/ProductCrud1Application.java
+++ ProductCRUD-1/src/main/java/com/example/ProductCrud1Application.java
@@ -2,12 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class ProductCrud1Application {
+public class ProductCrud1Application extends SpringBootServletInitializer {
 
+	//JAR
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCrud1Application.class, args);
 	}
+	
+	//WAR
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(ProductCrud1Application.class);
+	    }
 
 }
